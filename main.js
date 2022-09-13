5066b18990df55c985020dd587c815f28c316685
bruno.ramos <bruno.bragaramos@gmail.com>
2022-09-13 09:21:45 -0300

    update

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-if (document.querySelector('input[type="text"]:focus-visible')) {
-    document.querySelector('.search-bar').classList.add('search-bar-select')
-}
+document.querySelector('input[type="text"]').addEventListener('keydown', function(e) {
+    document.querySelector('.search-bar').classList.add('search-bar-focused');
+    if (e.target.value == '' && e.which == 8) {
+        document.querySelector('.search-bar').classList.remove('search-bar-focused');
+    }
+})
+
+
+document.querySelector('.svg-container').addEventListener('click', function(e) {
+    document.querySelector('.apps-container').classList.toggle('active');
+})
